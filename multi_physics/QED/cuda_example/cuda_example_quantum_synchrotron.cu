dc64481c62a498e65c50fc846be483bb558c0cae
Luca Fedeli <luca.fedeli@cea.fr>
2021-05-03 11:45:53 +0200

    completed QS pure CUDA test

--- multi_physics/QED/cuda_example/cuda_example_quantum_synchrotron.cu
+++ multi_physics/QED/cuda_example/cuda_example_quantum_synchrotron.cu
@@ -2,13 +2,14 @@
 
 #include <cstdio>
 
-// BREIT-WHEELER PAIR PRODUCTION
+// QUANTUM SYNCHROTRON EMISSION
 
 //Parameters of the test case
 const unsigned int how_many_particles = 20'000'000;
 const double dt_test= 1e-18;
 const double table_chi_min = 0.01;
 const double table_chi_max = 1000.0;
+const double table_frac_min = 1.0e-12;
 const int table_chi_size = 128;
 const int table_frac_size = 128;
 const int random_seed = 22051988;
@@ -37,10 +38,10 @@ auto generate_dndt_table(Real chi_min, Real chi_max, int chi_size)
         << ", " << chi_size <<"]...\n";
     std::cout.flush();
 
-    pxr_bw::dndt_lookup_table_params<Real> bw_params{chi_min, chi_max, chi_size};
+    pxr_qs::dndt_lookup_table_params<Real> qs_params{chi_min, chi_max, chi_size};
 
-	auto table = pxr_bw::dndt_lookup_table<
-        Real, Vector>{bw_params};
+	auto table = pxr_qs::dndt_lookup_table<
+        Real, Vector>{qs_params};
 
     table.generate();
 
@@ -48,7 +49,7 @@ auto generate_dndt_table(Real chi_min, Real chi_max, int chi_size)
 }
 
 /**
-* Generates the pair production lookup table
+* Generates the photon emission lookup table
 *
 * @tparam Real the floating point type to be used
 * @tparam Vector the vector type to be used
@@ -59,17 +60,18 @@ auto generate_dndt_table(Real chi_min, Real chi_max, int chi_size)
 * @return the lookup table
 */
 template <typename Real, typename Vector>
-auto generate_pair_table(Real chi_min, Real chi_max, int chi_size, int frac_size)
+auto generate_photon_emission_table(
+    Real chi_min, Real chi_max, Real frac_min, int chi_size, int frac_size)
 {
-    std::cout << "Preparing pair production table [" << get_type_name<Real>()
+    std::cout << "Preparing photon emission table [" << get_type_name<Real>()
         << ", " << chi_size << " x " << frac_size <<"]...\n";
     std::cout.flush();
 
-    pxr_bw::pair_prod_lookup_table_params<Real> bw_params{
-        chi_min, chi_max, chi_size, frac_size};
+    pxr_qs::photon_emission_lookup_table_params<Real> qs_params{
+        chi_min, chi_max, frac_min, chi_size, frac_size};
 
-	auto table = pxr_bw::pair_prod_lookup_table<
-        Real, Vector>{bw_params};
+	auto table = pxr_qs::photon_emission_lookup_table<
+        Real, Vector>{qs_params};
 
     table.template generate();
 
@@ -83,7 +85,7 @@ void fill_opt(int N, Real* opt_data, Real* unf_zero_one)
     int i = blockIdx.x*blockDim.x + threadIdx.x;
 
 	if (i < N){
-        opt_data[i] = pxr_bw::get_optical_depth<Real>(
+        opt_data[i] = pxr_qs::get_optical_depth<Real>(
             Real(1.0)-unf_zero_one[i]);
     }
 }
@@ -150,17 +152,18 @@ void evolve_opt(const int N,
         const auto& px = mom[ParticleData<Real>::idx(i,0)];
         const auto& py = mom[ParticleData<Real>::idx(i,1)];
         const auto& pz = mom[ParticleData<Real>::idx(i,2)];
-        const auto chi = pxr::chi_photon<Real, pxr::unit_system::SI>(
-            px, py, pz, ex[i], ey[i], ez[i], bx[i], by[i], bz[i]);
 
         const auto ppx = px/mec<Real>;
 	    const auto ppy = py/mec<Real>;
 	    const auto ppz = pz/mec<Real>;
-        const auto en = (sqrt(ppx*ppx + ppy*ppy + ppz*ppz))*mec2<Real>;
+        const auto ee =
+            pxr::compute_gamma_ele_pos<Real>(px, py, pz)*mec2<Real>;
 
-        pxr_bw::evolve_optical_depth<Real,TableType>(
-            en, chi, dt, opt[i],
-            ref_table);
+        const auto chi =
+            pxr::chi_ele_pos<Real, pxr::unit_system::SI>(
+                px, py ,pz, ex[i], ey[i], ez[i], bx[i], by[i], bz[i]);
+        pxr_qs::evolve_optical_depth<Real, TableType>(
+                ee, chi, dt, opt[i], ref_table);
     }
 }
 
@@ -198,8 +201,8 @@ evolve_optical_depth(
 
 template <typename Real, typename TableType>
 __global__
-void pair_gen(const int N,
-   const Real*__restrict__ mom,
+void phot_gen(const int N,
+   Real*__restrict__ mom,
    const Real*__restrict__ ex,
    const Real*__restrict__ ey,
    const Real*__restrict__ ez,
@@ -208,8 +211,7 @@ void pair_gen(const int N,
    const Real*__restrict__ bz,
    const Real*__restrict__ rand,
    const TableType ref_table,
-   Real*__restrict__ mom_ele,
-   Real*__restrict__ mom_pos)
+   Real*__restrict__ mom_phot)
 {
     int i = blockIdx.x*blockDim.x + threadIdx.x;
 
@@ -220,31 +222,31 @@ void pair_gen(const int N,
 
         const auto unf_zero_one = Real(1.0)-rand[i];
 
-        const auto chi = pxr::chi_photon<Real, pxr::unit_system::SI>(
+        const auto chi = pxr::chi_ele_pos<Real, pxr::unit_system::SI>(
             px, py, pz, ex[i], ey[i], ez[i], bx[i], by[i], bz[i]);
 
-        const auto pp = pxr_m::vec3<Real>{px, py, pz};
+        auto pp = pxr_m::vec3<Real>{px, py, pz};
 
-        auto e_mom = pxr_m::vec3<Real>{0,0,0};
-        auto p_mom = pxr_m::vec3<Real>{0,0,0};
+        auto e_phot = pxr_m::vec3<Real>{0,0,0};
 
-        pxr_bw::generate_breit_wheeler_pairs<Real, TableType, pxr::unit_system::SI>(
+        pxr_qs::generate_photon_update_momentum<Real, TableType, pxr::unit_system::SI>(
             chi, pp,
             unf_zero_one,
             ref_table,
-            e_mom, p_mom);
+            e_phot);
 
-        mom_ele[ParticleData<Real>::idx(i,0)] = e_mom[0];
-        mom_ele[ParticleData<Real>::idx(i,1)] = e_mom[1];
-        mom_ele[ParticleData<Real>::idx(i,2)] = e_mom[2];
-        mom_pos[ParticleData<Real>::idx(i,0)] = p_mom[0];
-        mom_pos[ParticleData<Real>::idx(i,1)] = p_mom[1];
-        mom_pos[ParticleData<Real>::idx(i,2)] = p_mom[2];
+        mom_phot[ParticleData<Real>::idx(i,0)] = e_phot[0];
+        mom_phot[ParticleData<Real>::idx(i,1)] = e_phot[1];
+        mom_phot[ParticleData<Real>::idx(i,2)] = e_phot[2];
+
+        mom[ParticleData<Real>::idx(i,0)] = pp[0];
+        mom[ParticleData<Real>::idx(i,1)] = pp[1];
+        mom[ParticleData<Real>::idx(i,2)] = pp[2];
     }
 }
 
 /**
-* Tests pair production
+* Tests
 *
 * @tparam Real the floating point type to be used
 * @tparam TableType the lookup table type
@@ -255,41 +257,43 @@ void pair_gen(const int N,
 */
 template <typename Real, typename TableType>
 std::pair<bool, double>
-generate_pairs(
+generate_photons(
     ParticleData<Real>& pdata,
     const TableType ref_table,
     curandGenerator_t& gen)
 {
     const auto N = pdata.num_particles;
-    std::vector<Real> t_ele_mom(
+    std::vector<Real> t_phot_mom(
         ParticleData<Real>::num_components*N);
-    std::vector<Real> t_pos_mom(
+    std::vector<Real> t_part_mom(
         ParticleData<Real>::num_components*N);
 
-    Real *rand, *ele_momentum, *pos_momentum;
+    Real *rand, *phot_momentum, *part_momentum;
     cudaMalloc(&rand,N*sizeof(Real));
-    cudaMalloc(&ele_momentum,
+    cudaMalloc(&phot_momentum,
         N*ParticleData<Real>::num_components*sizeof(Real));
-    cudaMalloc(&pos_momentum,
-        N*ParticleData<Real>::num_components*sizeof(Real));
+    cudaMalloc(&part_momentum,
+            N*ParticleData<Real>::num_components*sizeof(Real));
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
 
     cudaEventRecord(start);
     w_curandGenerateUniform(gen, rand, N);
-    pair_gen<<< (N+255)/256, 256 >>>(
+    phot_gen<<< (N+255)/256, 256 >>>(
         N,
         pdata.m_momentum,
         pdata.m_fields.Ex, pdata.m_fields.Ey, pdata.m_fields.Ez,
         pdata.m_fields.Bx, pdata.m_fields.By, pdata.m_fields.Bz,
-        rand, ref_table, ele_momentum, pos_momentum);
+        rand, ref_table, phot_momentum);
     cudaEventRecord(stop);
 
-    cudaMemcpy(t_ele_mom.data(), pdata.m_momentum,
+    cudaMemcpy(t_phot_mom.data(), phot_momentum,
         N*ParticleData<Real>::num_components*sizeof(Real), cudaMemcpyDeviceToHost);
-    cudaMemcpy(t_pos_mom.data(), pdata.m_momentum,
+
+    cudaMemcpy(t_part_mom.data(), pdata.m_momentum,
         N*ParticleData<Real>::num_components*sizeof(Real), cudaMemcpyDeviceToHost);
+
 
     cudaEventSynchronize(stop);
 
@@ -297,57 +301,12 @@ generate_pairs(
     cudaEventElapsedTime(&time, start, stop);
 
     cudaFree(rand);
-    cudaFree(ele_momentum);
-    cudaFree(pos_momentum);
+    cudaFree(phot_momentum);
+    cudaFree(part_momentum);
 
     return std::make_pair(
-        check(t_ele_mom, true, true) && check(t_pos_mom, true, true),
+        check(t_phot_mom, true, true) && check(t_part_mom, true, true),
         time);
-}
-
-template <typename Real>
-__global__
-void corr_mom(int N, Real* mom)
-{
-    int i = blockIdx.x*blockDim.x + threadIdx.x;
-
-	if (i < N){
-        const auto& px = mom[ParticleData<Real>::idx(i,0)];
-        const auto& py = mom[ParticleData<Real>::idx(i,1)];
-        const auto& pz = mom[ParticleData<Real>::idx(i,2)];
-
-        const auto gamma_gamma = pxr::compute_gamma_photon<Real>(px, py, pz);
-
-        const auto bb = Real(2.1);
-
-        if(gamma_gamma == Real(0.0) ){
-            const auto cc = bb/std::sqrt(Real(3.0));
-            mom[ParticleData<Real>::idx(i,0)] = cc*mec<Real>;
-            mom[ParticleData<Real>::idx(i,1)] = cc*mec<Real>;
-            mom[ParticleData<Real>::idx(i,2)] = cc*mec<Real>;
-        }
-        else if (gamma_gamma < Real(2.0)){
-            const auto cc = bb/gamma_gamma;
-            mom[ParticleData<Real>::idx(i,0)] *= cc;
-            mom[ParticleData<Real>::idx(i,1)] *= cc;
-            mom[ParticleData<Real>::idx(i,2)] *= cc;
-        }
-    }
-}
-
-
-/**
-* Corrects momenta which are too low to allow the decay of
-* a photon into a pair.
-*
-* @tparam Real the floating point type to be used
-* @param[in,out] pdata the particle data
-*/
-template<typename Real>
-void correct_low_momenta(ParticleData<Real>& pdata)
-{
-    const auto N = pdata.num_particles;
-    corr_mom<<<(N+255)/256, 256>>>(N, pdata.m_momentum);
 }
 
 
@@ -363,7 +322,6 @@ void do_test(curandGenerator_t& rand_pool)
     auto particle_data = create_particles<Real>(
         how_many_particles,
         P_min, P_max, E_min, E_max, B_min, B_max, rand_pool);
-    correct_low_momenta(particle_data);
 
     const auto dndt_table =
         generate_dndt_table<Real, ThrustDeviceWrapper<Real>>(
@@ -371,15 +329,16 @@ void do_test(curandGenerator_t& rand_pool)
             table_chi_max,
             table_chi_size);
 
-    const auto pair_table =
-        generate_pair_table<Real,ThrustDeviceWrapper<Real>>(
+    const auto phot_em_table =
+    generate_photon_emission_table<Real,ThrustDeviceWrapper<Real>>(
             table_chi_min,
             table_chi_max,
+            table_frac_min,
             table_chi_size,
             table_frac_size);
 
     const auto dndt_table_view = dndt_table.get_view();
-    const auto pair_table_view = pair_table.get_view();
+    const auto phot_em_table_view = phot_em_table.get_view();
 
     bool fill_opt_success = false; double fill_opt_time = 0.0;
     std::tie(fill_opt_success, fill_opt_time) =
@@ -394,12 +353,12 @@ void do_test(curandGenerator_t& rand_pool)
     std::cout << ( evolve_opt_success? "[ OK ]":"[ FAIL ]" )
         << "  Evolve Optical Depth : " << evolve_opt_time << " ms" << std::endl;
 
-    bool pair_prod_success = false; double pair_prod_time = 0.0;
-    std::tie(pair_prod_success, pair_prod_time) =
-        generate_pairs<Real>(
-            particle_data, pair_table_view, rand_pool);
-    std::cout << ( pair_prod_success? "[ OK ]":"[ FAIL ]" )
-        << "  Pair Production : " << pair_prod_time << " ms" << std::endl;
+    bool phot_em_success = false; double phot_em_time = 0.0;
+    std::tie(phot_em_success, phot_em_time) =
+        generate_photons<Real>(
+            particle_data, phot_em_table_view, rand_pool);
+    std::cout << ( phot_em_success? "[ OK ]":"[ FAIL ]" )
+        << "  Photon Emission : " << phot_em_time << " ms" << std::endl;
 
     free_particles(particle_data);
 }
